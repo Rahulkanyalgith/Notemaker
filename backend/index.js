a56cb859c0fe4abeaeee62ddffbab884a02123c5
Rahulkanyalgith <141804833+Rahulkanyalgith@users.noreply.github.com>
2024-07-06 19:23:49 +0530

    debugg

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,24 @@
-import connectToMongo from './database/db.js';
+import connectToMongo from "./database/db.js";
 import express from 'express';
-import auth from './routes/auth.js'    
-import notes from './routes/notes.js'
+import auth from './routes/auth.js';
+import notes from './routes/notes.js';
 import cors from 'cors'
-
 
 connectToMongo();
 const app = express()
-const port = 4000
-    
+const port = 4000;
 
-app.use(express.json())
-app.use(cors())
+//* middleware
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth' ,auth)
-app.use('/api/notes' ,notes)
-    
+//* Available Route 
+app.get('/', (req, res) => {
+    res.json("eNotebook backend Api")
+});
+app.use('/api/auth', auth);
+app.use('/api/notes', notes);
+
 app.listen(port, () => {
-    
     console.log(`Example app listening at http://localhost:${port}`)
 })
